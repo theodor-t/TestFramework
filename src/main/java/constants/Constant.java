b484d5d30be30bc98ff5ab1bf293c81d9764a564
Theodor <54949643+theodorerlax@users.noreply.github.com>
2022-11-29 14:28:40 +0200

    Add files via upload

---
+++ src/main/java/constants/Constant.java
@@ -0,0 +1,32 @@
+package constants;
+
+public class Constant {
+    public static class TimeoutVariable {
+        //Set timeout
+        public static final int IMPLICIT_WAIT = 4;
+        public static final int EXPLICIT_WAIT = 10;
+    }
+
+    public static class Urls {
+        public static final String HOME_PAGE = "https://www.ep-zone.net/";
+        public static final String COMMUNITY_PAGE = "https://community.ep-zone.net/";
+        public static final String LOGIN_PAGE = "https://www.ep-zone.net/login";
+        public static final String EPL_PAGE = "https://epl.ep-zone.net/";
+        public static final String EXIMA_HOMEPAGE = "https://exima.usalinksystem.net/";
+    }
+
+    public static class Accounts {
+        //Single account
+        public static final String EMAIL_SINGLEACC = "buyer98@mailinator.com";
+        public static final String PASSWORD_SINGLEACC = "Tester123";
+        //Multi account
+        public static final String EMAIL_MULTIACC = "full@uls.com";
+        public static final String PASSWORD_MULTIACC = "Pa$$w0rd!";
+        //EPL Account
+        public static final String EMAIL_EPL_ACCOUNT = "shipper@uls.com";
+        public static final String PASSWORD_EPL_ACCOUNT = "Pa$$w0rd!";
+        //EXIMA Account
+        public static final String EMAIL_EXIMA = "importer100@mailinator.com";
+        public static final String PASSWORD_EXIMA = "Test123!";
+    }
+}
